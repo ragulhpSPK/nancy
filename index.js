b0a3d7e9fd2d2044bcf6ef383938428173e88d76
ragulhpSPK <ragulhp27@gmail.com>
2023-06-17 18:39:03 +0530

    lordofrings

--- index.js
+++ index.js
@@ -1,20 +1,145 @@
-// index.js
+// // index.js
+// const express = require("express");
+
+// const app = express();
+// const PORT = 4000;
+
+// app.listen(PORT, () => {
+//   console.log(`API listening on PORT ${PORT} `);
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Hey this is my API running 🥳");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("This is my about route..... ");
+// });
+
+// // Export the Express API
+// module.exports = app;
+
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
-const PORT = 4000;
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
+app.listen(8080, () => {
+  console.log("narniya");
 });
-
 app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
+  res.send("ehlrjgi");
 });
+// const userSchema = mongoose.model(
+//   "user",
+//   new mongoose.Schema(
+//     {
+//       name: String,
+//       age: Number,
+//       day: Array,
+//       moredetails: [
+//         {
+//           address: String,
+//           street: String,
+//         },
+//       ],
+//     },
+//     { collection: "user" }
+//   )
+// );
+// const productSchema = mongoose.model(
+//   "product",
+//   new mongoose.Schema(
+//     {
+//       name: String,
+//       userId: {
+//         type: mongoose.Schema.ObjectId,
+//         ref: "user",
+//       },
+//     },
+//     { collection: "product" }
+//   )
+// );
+// mongoose.connect("mongodb://127.0.0.1:27017/apps").then(() => {
+//   app.listen(8080, (err) => {
+//     if (!err) {
+//       console.log("server started at 8080");
+//     }
+//   });
+// });
 
-app.get("/about", (req, res) => {
-  res.send("This is my about route..... ");
-});
+//  {
+//       age: { $gte: 24, $lte: 70 },
+//     }
+// app.get("/", async (req, res) => {
+//   try {
+//     const result = await productSchema
+//       .find()
+//       .select({ name: 0 })
+//       .populate("userId", { {moredetails: 0 } });
 
-// Export the Express API
-module.exports = app;
+//     return res.status(200).send({ data: result });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// app.get("/", async (req, res) => {
+//   try {
+//     const result = await productSchema
+//       .find({ $neq: { _id: "648d9ad19c9515755a372b20" } })
+//       .populate("userId");
+//     return res.status(200).send({ data: result });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// app.get("/:name", async (req, res) => {
+//   try {
+//     let where =
+//       req.params.name === "dashboard" ? { moredetails: 0 } : { moredetails: 1 };
+
+//     const result = await productSchema
+//       .find()
+//       .select({ name: 0 })
+//       .populate("userId", where);
+
+//     return res.status(200).send({ data: result });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// app.post("/", async (req, res) => {
+//   try {
+//     const user = new userSchema({
+//       name: "saniya",
+//       age: 24,
+//       day: [1, 2, 3],
+//       oredetails: { address: "karur", street: "saniya" },
+//     });
+//     await user.save();
+//     const product = new productSchema({
+//       name: "parota",
+//       userId: "648d9ad19c9515755a372b20",
+//     });
+//     await product.save();
+//     return res.status(200).send({ data: user });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// app.put("/update", async (req, res) => {
+//   try {
+//     const result = await userSchema.findByIdAndUpdate(
+//       {
+//         _id: "648d98fc3bd30b99ca6fa8a2",
+//       },
+//       { $pull: { moredetails: {} } }
+//     );
+//     return res.status(200).send({ data: "user" });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
