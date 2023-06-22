4e15e53c5bf7d7aa6e720e77e9c39da2862e0ca2
ragulhpSPK <ragulhp27@gmail.com>
2023-06-22 14:56:01 +0530

    gj

--- index.js
+++ index.js
@@ -1,63 +1,54 @@
-// // index.js
-// const express = require("express");
-
-// const app = express();
-// const PORT = 4000;
-
-// app.listen(PORT, () => {
-//   console.log(`API listening on PORT ${PORT} `);
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("Hey this is my API running 🥳");
-// });
-
-// app.get("/about", (req, res) => {
-//   res.send("This is my about route..... ");
-// });
-
-// // Export the Express API
-// module.exports = app;
-
+// index.js
 const express = require("express");
-import mongoose from "mongoose";
-const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello");
+const app = express();
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
 });
 
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
+
+// // Export the Express API
 module.exports = app;
-// const userSchema = mongoose.model(
-//   "user",
-//   new mongoose.Schema(
-//     {
-//       name: String,
-//       age: Number,
-//       day: Array,
-//       moredetails: [
-//         {
-//           address: String,
-//           street: String,
-//         },
-//       ],
-//     },
-//     { collection: "user" }
-//   )
-// );
-// const productSchema = mongoose.model(
-//   "product",
-//   new mongoose.Schema(
-//     {
-//       name: String,
-//       userId: {
-//         type: mongoose.Schema.ObjectId,
-//         ref: "user",
-//       },
-//     },
-//     { collection: "product" }
-//   )
-// );
+
+const userSchema = mongoose.model(
+  "user",
+  new mongoose.Schema(
+    {
+      name: String,
+      age: Number,
+      day: Array,
+      moredetails: [
+        {
+          address: String,
+          street: String,
+        },
+      ],
+    },
+    { collection: "user" }
+  )
+);
+const productSchema = mongoose.model(
+  "product",
+  new mongoose.Schema(
+    {
+      name: String,
+      userId: {
+        type: mongoose.Schema.ObjectId,
+        ref: "user",
+      },
+    },
+    { collection: "product" }
+  )
+);
 mongoose
   .connect(
     "mongodb+srv://ragulhp27:ragulhp2704@cluster0.uxfxhya.mongodb.net/antman?retryWrites=true&w=majority"
@@ -97,52 +88,52 @@ mongoose
 //   }
 // });
 
-// app.get("/:name", async (req, res) => {
-//   try {
-//     let where =
-//       req.params.name === "dashboard" ? { moredetails: 0 } : { moredetails: 1 };
+app.get("/:name", async (req, res) => {
+  try {
+    let where =
+      req.params.name === "dashboard" ? { moredetails: 0 } : { moredetails: 1 };
 
-//     const result = await productSchema
-//       .find()
-//       .select({ name: 0 })
-//       .populate("userId", where);
+    const result = await productSchema
+      .find()
+      .select({ name: 0 })
+      .populate("userId", where);
 
-//     return res.status(200).send({ data: result });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
+    return res.status(200).send({ data: result });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-// app.post("/", async (req, res) => {
-//   try {
-//     const user = new userSchema({
-//       name: "saniya",
-//       age: 24,
-//       day: [1, 2, 3],
-//       oredetails: { address: "karur", street: "saniya" },
-//     });
-//     await user.save();
-//     const product = new productSchema({
-//       name: "parota",
-//       userId: "648d9ad19c9515755a372b20",
-//     });
-//     await product.save();
-//     return res.status(200).send({ data: user });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
+app.post("/", async (req, res) => {
+  try {
+    const user = new userSchema({
+      name: "saniya",
+      age: 24,
+      day: [1, 2, 3],
+      oredetails: { address: "karur", street: "saniya" },
+    });
+    await user.save();
+    const product = new productSchema({
+      name: "parota",
+      userId: "648d9ad19c9515755a372b20",
+    });
+    await product.save();
+    return res.status(200).send({ data: user });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-// app.put("/update", async (req, res) => {
-//   try {
-//     const result = await userSchema.findByIdAndUpdate(
-//       {
-//         _id: "648d98fc3bd30b99ca6fa8a2",
-//       },
-//       { $pull: { moredetails: {} } }
-//     );
-//     return res.status(200).send({ data: "user" });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
+app.put("/update", async (req, res) => {
+  try {
+    const result = await userSchema.findByIdAndUpdate(
+      {
+        _id: "648d98fc3bd30b99ca6fa8a2",
+      },
+      { $pull: { moredetails: {} } }
+    );
+    return res.status(200).send({ data: "user" });
+  } catch (err) {
+    console.log(err);
+  }
+});
